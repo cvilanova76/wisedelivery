f87fa4f5d5ee3438e3275a727fd474aad171ed40
cvilanova76 <carolinavilanova@gmail.com>
2023-05-05 18:36:09 -0300

    projeto criado; primeiro commit

---
+++ src/main/java/br/com/ccvn/wisedelivery/WisedeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.ccvn.wisedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisedeliveryApplication.class, args);
+	}
+
+}
